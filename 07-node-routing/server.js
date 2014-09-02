d3f4b400c6280c621127d5ec94fdff21acb85a47
Chris Sevilleja <chris@scotch.io>
2014-09-02 15:10:58 -0700

    adding files for ch 7

--- 07-node-routing/server.js
+++ 07-node-routing/server.js
@@ -11,26 +11,59 @@ app.get('/', function(req, res) {
 });
 
 	// get an instance of the router
-	var router = express.Router();
+	var adminRouter = express.Router();
+
+	   // route middleware that will happen on every request
+    adminRouter.use(function(req, res, next) {
+
+        // log each request to the console
+        console.log(req.method, req.url);
+
+        // continue doing what we were doing and go to the route
+        next(); 
+    });
+
+    // route middleware to validate :name
+    adminRouter.param('name', function(req, res, next, name) {
+        // do validation on name here
+        // blah blah validation
+        // log something so we know its working
+        console.log('doing name validations on ' + name);
+
+        // once validation is done save the new item in the req
+        req.name = name;
+        // go to the next thing
+        next(); 
+    });
+
+    // route with parameters (http://localhost:1337/hello/:name)
+    adminRouter.get('/hello/:name', function(req, res) {
+        res.send('hello ' + req.name + '!');
+    });
 
 	// create routes for the admin section
 	// admin main page. the dashboard
-	router.get('/', function(req, res) {
-		res.render('admin.dashboard');
+	adminRouter.get('/', function(req, res) {
+		res.send('I am the dashboard!');
 	});
 
 	// users page
-	router.get('/users', function(req, res) {
-		res.render('admin.users');
+	adminRouter.get('/users', function(req, res) {
+		res.send('I show all the users!');
 	});
 
+	   // route with parameters (http://localhost:1337/admin/users/:name)
+    adminRouter.get('/users/:name', function(req, res) {
+        res.send('hello ' + req.params.name + '!');
+    });
+
 	// posts page
-	router.get('/posts', function(req, res) {
-		res.render('admin.posts');
+	adminRouter.get('/posts', function(req, res) {
+		res.send('I show all the posts!');
 	});
 
 	// apply the routes to our application
-	app.use('/', router);
+	app.use('/admin', adminRouter);
 
 // start the server
 app.listen(1337);
