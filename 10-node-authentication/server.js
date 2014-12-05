063d7dd3169149d34c2b0c4991092b8a7c7908f1
Chris Sevilleja <chris@scotch.io>
2014-12-05 09:06:11 -0800

    adding authentication and middleware

--- 10-node-authentication/server.js
+++ 10-node-authentication/server.js
@@ -32,28 +32,76 @@ app.get('/', function(req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
-apiRouter.post('/authenticate', function(req, res) {
-	// authentication goes here
-	// find the user by their username
-	User.find({ username: req.body.username }, function(err, user) {
-		if (err) res.send(err);
-		
-		// validate their password
-		var validPassword = user.comparePassword(req.body.password);
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
 
-		// if the password is valid, generate a token
-		if (validPassword)
+  // find the user
+  User.findOne({
+    username: req.body.username
+  }, function(err, user) {
 
-			// return as JSON
-	});
+    if (err) throw err;
+
+    // no user with that username was found
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      var validPassword = user.comparePassword(req.body.password);
+      if (!validPassword) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
 });
 
-// middleware to use for all requests
-apiRouter.use(function(req, res, next) {
+// route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
 	// do logging
 	console.log('Somebody just came to our app!');
 
-	next(); // make sure we go to the next routes and don't stop here
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err)
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      else
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.json({ success: false, message: 'No token provided.' });
+    
+  }
+
+  next(); // make sure we go to the next routes and don't stop here
+  next();
 });
 
 // test route to make sure everything is working 
