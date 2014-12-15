d90939b3f3c8d57bada11cec3229175e7bc9138d
Chris Sevilleja <chris@scotch.io>
2014-12-14 20:33:43 -0800

    what is happening

--- 14-angular-service/public/js/app.js
+++ 14-angular-service/public/js/app.js
@@ -1,1 +1,6 @@
-angular.module('userApp', ['ngAnimate', 'appRoutes', 'appController', 'userRoutes', 'userController', 'userService']);
+angular.module('userApp', ['ngAnimate', 'appRoutes', 'appController', 'authService', 'userRoutes', 'userController', 'userService'])
+
+// configure application on run
+.run(['$location', 'Auth', function($location, Auth) {
+	
+}]);
