c4b8de4fbc1d0e613f24d92955806891458e815d
Chris Sevilleja <chris@scotch.io>
2015-01-05 23:58:56 -0800

    upgrading to 1.3.8

--- 13-angular-animating/public/js/app.js
+++ 13-angular-animating/public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('routerApp', ['routerRoutes'])
+angular.module('routerApp', ['routerRoutes', 'ngAnimate'])
 
 // create the controller and inject Angular's 
 // this will be the controller for the ENTIRE site
