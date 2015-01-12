47bb850977ad5cb57cc3ce9648bf694b6d0409ab
Chris Sevilleja <chris@scotch.io>
2015-01-11 19:06:24 -0800

    doing things

--- 17-user-crm/public/app/controllers/mainCtrl.js
+++ 17-user-crm/public/app/controllers/mainCtrl.js
@@ -1,12 +1,18 @@
 angular.module('mainCtrl', [])
 
-.controller('mainController', function($http, $location, Auth) {
+.controller('mainController', function($rootScope, $location, Auth) {
 
 	var vm = this;
 
-	// get user information on page load
-	// could also be placed in a .run()
+	// get info if a person is logged in
 	vm.loggedIn = Auth.isLoggedIn();
+
+	// check to see if a user is logged in on every request
+	$rootScope.$on('$routeChangeStart', function() {
+		vm.loggedIn = Auth.isLoggedIn();
+	});
+
+	// get user information on page load
 	Auth.getUser()
 		.then(function(data) {
 			console.log(data);
@@ -30,6 +36,7 @@ angular.module('mainCtrl', [])
 
 	// function to handle logging out
 	vm.doLogout = function() {
+		console.log('stuff');	
 		Auth.logout();
 		$location.path('/');
 	};
