ecd6ea453e93618063e1cd67efe077310ea7e376
Justin Page <xjustinpagex@gmail.com>
2015-01-26 16:27:53 -0800

    indentation on app.js
    added indentation to match app flow structure.

--- 12-angular-routing/public/js/app.js
+++ 12-angular-routing/public/js/app.js
@@ -6,8 +6,8 @@ angular.module('routerApp', ['routerRoutes'])
 
 	var vm = this;
 
-    // create a bigMessage variable to display in our view
-    vm.bigMessage = 'A smooth sea never made a skilled sailor.';
+    	// create a bigMessage variable to display in our view
+    	vm.bigMessage = 'A smooth sea never made a skilled sailor.';
 
 })
 
@@ -24,7 +24,7 @@ angular.module('routerApp', ['routerRoutes'])
 
 	var vm = this;
 
-    vm.message = 'Look! I am an about page.';
+    	vm.message = 'Look! I am an about page.';
 })
 
 // contact page controller
@@ -32,5 +32,5 @@ angular.module('routerApp', ['routerRoutes'])
 
 	var vm = this;
 
-    vm.message = 'Contact us! JK. This is just a demo.';
+    	vm.message = 'Contact us! JK. This is just a demo.';
 });
